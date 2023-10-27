0239472ef3083e5d386c6ca73448662df459deec
Uzielmf <Uziel.munozf@gmail.com>
2023-10-27 15:04:25 -0600

    se modifica el formato de la fecha

--- src/components/NextFiveDays.jsx
+++ src/components/NextFiveDays.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function NextFiveDays({setGrados,dataWeek}) {
   
-  const options = {  weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+  const options = { day: 'numeric', month: 'short', year: 'numeric'};
 
   //datos dia1
   const min1 = dataWeek==''?'':dataWeek.list[1].main.temp_min
@@ -16,25 +16,33 @@ function NextFiveDays({setGrados,dataWeek}) {
   const max2 = dataWeek==''?'':dataWeek.list[9].main.temp_max
   const img2 = dataWeek==''?'':dataWeek.list[9].weather[0].icon
   const date2 = dataWeek==''?'':dataWeek.list[9].dt_txt
-  
+  const fecha2 = new Date(date2)
+  const fechaFormateada2 = fecha2.toLocaleDateString('en-US', options);
 
   //datos dia3
   const min3 = dataWeek==''?'':dataWeek.list[17].main.temp_min
   const max3 = dataWeek==''?'':dataWeek.list[17].main.temp_max
   const img3 = dataWeek==''?'':dataWeek.list[17].weather[0].icon
   const date3 = dataWeek==''?'':dataWeek.list[17].dt_txt
+  const fecha3 = new Date(date3)
+  const fechaFormateada3 = fecha3.toLocaleDateString('en-US', options);
+
 
   //datos dia4
   const min4 = dataWeek==''?'':dataWeek.list[25].main.temp_min
   const max4 = dataWeek==''?'':dataWeek.list[25].main.temp_max
   const img4 = dataWeek==''?'':dataWeek.list[25].weather[0].icon
   const date4 = dataWeek==''?'':dataWeek.list[25].dt_txt
+  const fecha4 = new Date(date4)
+  const fechaFormateada4 = fecha4.toLocaleDateString('en-US', options);
 
   //datos dia5
   const min5 = dataWeek==''?'':dataWeek.list[33].main.temp_min
   const max5 = dataWeek==''?'':dataWeek.list[33].main.temp_max
   const img5 = dataWeek==''?'':dataWeek.list[33].weather[0].icon
   const date5 = dataWeek==''?'':dataWeek.list[33].dt_txt
+  const fecha5 = new Date(date5)
+  const fechaFormateada5 = fecha5.toLocaleDateString('en-US', options);
 
   //  console.log(dataWeek==null?dataWeek.list[1].main.temp_min:'');
   return (
@@ -53,7 +61,7 @@ function NextFiveDays({setGrados,dataWeek}) {
                 </div>
             </Box >
             <Box className='card-day'>
-            <p>{date2}</p>
+            <p>{fechaFormateada2}</p>
             <img src={`./img/${img2}.png`} alt="" className='img-week'/>
             <div className='grados-week'>
                   <p>{Math.round(max2)}</p>
@@ -61,7 +69,7 @@ function NextFiveDays({setGrados,dataWeek}) {
                 </div>
             </Box>
             <Box className='card-day'>
-            <p>{date3}</p>
+            <p>{fechaFormateada3}</p>
             <img src={`./img/${img3}.png`} alt="" className='img-week'/>
             <div className='grados-week'>
                   <p>{Math.round(max3)}</p>
@@ -69,7 +77,7 @@ function NextFiveDays({setGrados,dataWeek}) {
                 </div>
             </Box>
             <Box className='card-day'>
-            <p>{date4}</p>
+            <p>{fechaFormateada4}</p>
             <img src={`./img/${img4}.png`} alt="" className='img-week'/>
             <div className='grados-week'>
                   <p>{Math.round(max4)}</p>
@@ -77,7 +85,7 @@ function NextFiveDays({setGrados,dataWeek}) {
                 </div>
             </Box>
             <Box className='card-day'>
-            <p>{date5}</p>
+            <p>{fechaFormateada5}</p>
             <img src={`./img/${img5}.png`} alt="" className='img-week'/>
             <div className='grados-week'>
                   <p>{Math.round(max5)}</p>
